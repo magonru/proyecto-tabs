cb274ea9b40b728189d912e2eab1ca0a858b2495
Alvaro Gonzales <mauriciogr999@gmail.com>
2025-10-24 19:39:29 -0500

    Actualizando README.md

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # proyecto-tabs
+https://github.com/magonru/proyecto-tabs
--- main.js
+++ main.js
@@ -7,7 +7,9 @@ contador = 1;
 const tabsInfo = [
     {id: 0, title: 'Primera pestaña', text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet, tempore doloremque. Incidunt dicta quia deleniti eaque? Libero dignissimos dolorum perferendis magnam consectetur sed ratione dolor mollitia incidunt expedita ipsam doloribus eos modi accusantium inventore odit voluptatum esse magni, deleniti voluptates?'},
     {id: 1, title: 'Segunda pestaña', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo nemo tenetur vitae, modi officiis non labore fuga deserunt tempore eum fugit incidunt laudantium omnis? Nihil possimus, iste quis molestias est deserunt? Molestias nostrum fugit illum, doloribus vitae sequi?'},
-    {id: 2, title: 'Tercera pestaña', text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, culpa vero magnam cum quisquam dolores pariatur eligendi non mollitia dolorum reiciendis corrupti voluptates fuga! Rerum, dolorum dolores voluptates quam officiis adipisci nostrum accusamus doloremque harum assumenda repellat, cum dolore, ea repudiandae expedita a nemo!'}
+    {id: 2, title: 'Tercera pestaña', text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, culpa vero magnam cum quisquam dolores pariatur eligendi non mollitia dolorum reiciendis corrupti voluptates fuga! Rerum, dolorum dolores voluptates quam officiis adipisci nostrum accusamus doloremque harum assumenda repellat, cum dolore, ea repudiandae expedita a nemo!'},
+    {id: 3, title: 'Cuarta pestaña', text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, voluptas perspiciatis necessitatibus aspernatur totam inventore, id natus quasi voluptatem dolore vel ullam obcaecati laborum iusto alias maiores in?'}
+    
 ];
 
 
